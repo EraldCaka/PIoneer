44b6b6c07e07874b6bcff40311335fb918cb2484
EraldCaka <eraldcaka47@gmail.com>
2026-03-05 00:49:23 -0800

    testing

---
+++ pioneer.go
@@ -0,0 +1,14 @@
+package pioneer
+
+import (
+	"os"
+
+	"github.com/EraldCaka/PIoneer/pkg/config"
+	"github.com/EraldCaka/PIoneer/pkg/handlers/pioneer"
+)
+
+type Device = config.Device
+
+func New(file *os.File) (Device, error) {
+	return pioneer.New(file)
+}
